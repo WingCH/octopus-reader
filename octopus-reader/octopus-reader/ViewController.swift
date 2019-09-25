99c758d71a8bf2433d3efccd5f57c1794ff1c89f
WingCH <g1191993@study.ouhk.edu.hk>
2019-09-25 21:59:35 +0800

    Initial project

---
+++ octopus-reader/octopus-reader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  octopus-reader
+//
+//  Created by CHAN Hong Wing on 25/9/2019.
+//  Copyright © 2019 CHAN Hong Wing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
