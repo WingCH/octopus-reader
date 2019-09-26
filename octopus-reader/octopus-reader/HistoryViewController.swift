9be2e28c294851d5e00785bf1019429b699ac7df
WingCH <g1191993@study.ouhk.edu.hk>
2019-09-26 22:44:48 +0800

    remove cell

--- octopus-reader/octopus-reader/HistoryViewController.swift
+++ octopus-reader/octopus-reader/HistoryViewController.swift
@@ -19,14 +19,86 @@ class HistoryCell: UITableViewCell{
 class HistoryViewController: UITableViewController, NSFetchedResultsControllerDelegate  {
     
     
-    var records: [Records] = []
+    var records: [Records] = [] {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
     var fetchResultController: NSFetchedResultsController<Records>!
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getData()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return records.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "historyCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! HistoryCell
+        
+        // Configure the cell...
+        cell.balance?.text = "\(records[indexPath.row].balance)"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = " HH:mm:ss dd-MM-yyyy"
+        
+        cell.date?.text = "\( dateFormatter.string(from: records[indexPath.row].create_date!))"
+        
+        
+        return cell
+    }
+    
+    
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data store
+            if let appDelegate = (UIApplication.shared.delegate as? AppDelegate) {
+                let context = appDelegate.persistentContainer.viewContext
+                let restaurantToDelete = self.fetchResultController.object(at: indexPath)
+                context.delete(restaurantToDelete)
+                appDelegate.saveContext()
+                getData()
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView,
+             didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
 
-        // Fetch data from data store
+    
+    func getData(){
         let fetchRequest: NSFetchRequest<Records> = Records.fetchRequest()
         let sortDescriptor = NSSortDescriptor(key: "create_date", ascending: false)
         fetchRequest.sortDescriptors = [sortDescriptor]
@@ -46,81 +118,5 @@ class HistoryViewController: UITableViewController, NSFetchedResultsControllerDe
             }
         }
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return records.count
-    }
-
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let cellIdentifier = "historyCell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! HistoryCell
-        
-        // Configure the cell...
-        cell.balance?.text = "\(records[indexPath.row].balance)"
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = " HH:mm:ss dd-MM-yyyy"
-        
-        cell.date?.text = "\( dateFormatter.string(from: records[indexPath.row].create_date!))"
-        
-
-        return cell
-    }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
